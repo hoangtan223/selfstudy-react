a8d4d73947ce0974439eb6c02b9eda4f46e2c1a9
Tanbui <hoangtan223@gmail.com>
2017-05-26 00:37:59 +0700

    First init for countdownchamp app

---
+++ countdownchamp/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import Clock from './Clock.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25th, 2017'
+    }
+  }
+
+  changeDeadline() {
+    this.setState({
+        deadline: this.state.newDeadline
+    })
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <div className="App-title">Countdown to {this.state.deadline}</div>
+        <Clock />
+        <div>
+          <input
+            placeholder="new date"
+            onChange={event => this.setState({
+              newDeadline: event.target.value
+            })}
+            />
+          <button onClick={() => this.changeDeadline()}>Submit</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
