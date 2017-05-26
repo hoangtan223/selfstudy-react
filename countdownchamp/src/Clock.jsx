3ed616e2647e945d2242f55f3ac4b3e49a89ca79
Tanbui <hoangtan223@gmail.com>
2017-05-26 11:15:19 +0700

    Add leading 0 helper method

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -21,6 +21,10 @@ class Clock extends Component {
     setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
   }
 
+  leading0(num) {
+    return num < 10 ? '0' + num : num;
+  }
+
   getTimeUntil(deadline){
     const time = Date.parse(deadline) - Date.parse(new Date());
     const seconds = Math.floor((time/1000)%60);
@@ -44,10 +48,10 @@ class Clock extends Component {
   render() {
   return(
     <div>
-      <div className="Clock-days">{this.state.days} Days</div>
-      <div className="Clock-hours">{this.state.hours} Hours</div>
-      <div className="Clock-minutes">{this.state.minutes} Minutes</div>
-      <div className="Clock-seconds">{this.state.seconds} Seconds</div>
+      <div className="Clock-days">{this.leading0(this.state.days)} Days</div>
+      <div className="Clock-hours">{this.leading0(this.state.hours)} Hours</div>
+      <div className="Clock-minutes">{this.leading0(this.state.minutes)} Minutes</div>
+      <div className="Clock-seconds">{this.leading0(this.state.seconds)} Seconds</div>
     </div>
   )}
 }
