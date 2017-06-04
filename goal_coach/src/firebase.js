4ed3591efa04216f9250b32b7da505eac24a5673
Tanbui <hoangtan223@gmail.com>
2017-06-05 02:17:31 +0700

    Sign up with firebase

---
+++ goal_coach/src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAj-Ri_rczlftXSj-SEMQ-NBrgNqngZ47c",
+    authDomain: "goalcoach-a3df1.firebaseapp.com",
+    databaseURL: "https://goalcoach-a3df1.firebaseio.com",
+    projectId: "goalcoach-a3df1",
+    storageBucket: "goalcoach-a3df1.appspot.com",
+    messagingSenderId: "620999627387"
+  };
+
+export const firebaseApp = firebase.initializeApp(config);
--- goal_coach/src/index.js
+++ goal_coach/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
 import App from './components/App.jsx';
 import SignIn from './components/Signin.jsx';
 import SignUp from './components/Signup.jsx';
 
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log('user has signed in or up', user);
+  } else {
+    console.log('user has signed out or still needs to sign in');
+  }
+})
+
 ReactDOM.render(
-  <div>App</div>, document.getElementById('root')
+  <Router path="/" history={browserHistory}>
+    <Route path="/app" component={App} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+  </Router>, document.getElementById('root')
 )
