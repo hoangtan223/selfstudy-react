22a93a55ce8ffbfb613a7d6160ce85db08ad8523
Tanbui <hoangtan223@gmail.com>
2017-06-05 01:07:37 +0700

    Init commit for goal_coach app

---
+++ goal_coach/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import SignIn from './components/Signin.jsx';
+import SignUp from './components/Signup.jsx';
+
+ReactDOM.render(
+  <div>App</div>, document.getElementById('root')
+)
